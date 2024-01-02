a4b11306cd4ad26f1fb60039ea06adee9c675a60
hfhbd <hfhbd@users.noreply.github.com>
2024-01-02 11:49:36 +0100

    Removed unused dependencies

--- gradle/build-logic/build.gradle.kts
+++ gradle/build-logic/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 
 dependencies {
     implementation(libs.plugins.kotlin.jvm.dep)
-    implementation(libs.plugins.kotlin.serialization.dep)
 }
 
 val Provider<PluginDependency>.dep: Provider<String> get() = map { "${it.pluginId}:${it.pluginId}.gradle.plugin:${it.version}" }
