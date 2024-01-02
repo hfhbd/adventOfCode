b81d49522ced1e89746fbd70562d80e0ee3c6419
hfhbd <hfhbd@users.noreply.github.com>
2024-01-02 14:14:07 +0100

    Reproducer: Add maven-publish

--- gradle/build-logic/src/main/kotlin/setup.gradle.kts
+++ gradle/build-logic/src/main/kotlin/setup.gradle.kts
@@ -1,9 +1,22 @@
 plugins {
     kotlin("jvm")
+    id("maven-publish")
 }
 
 kotlin.jvmToolchain(8)
 
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    repositories {
+        maven(url = "https://maven.pkg.github.com/hfhbd/advendOfCode") {
+            name = "GitHubPackages"
+            credentials(PasswordCredentials::class)
+        }
+    }
+    publications.register<MavenPublication>("gpr") {
+        from(components["java"])
+    }
 }
