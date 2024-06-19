b2e7b9db66e4c31216d78d13a74d9c969d8087bf
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-06-19 07:35:31 +0200

    Explicit set the file permissions (#56)

--- gradle/build-logic/src/main/kotlin/setup.gradle.kts
+++ gradle/build-logic/src/main/kotlin/setup.gradle.kts
@@ -50,4 +50,10 @@ signing {
 tasks.withType<AbstractArchiveTask>().configureEach {
     isPreserveFileTimestamps = false
     isReproducibleFileOrder = true
+    filePermissions {
+        unix(644)
+    }
+    dirPermissions {
+        unix(755)
+    }
 }
