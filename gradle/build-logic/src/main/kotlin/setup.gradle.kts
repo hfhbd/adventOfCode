14093b4c330aa47fbee1669e5b9805ed2f40de57
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-09-29 16:07:15 +0200

    Use sigstore to sign the artifacts

--- gradle/build-logic/src/main/kotlin/setup.gradle.kts
+++ gradle/build-logic/src/main/kotlin/setup.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm")
     id("maven-publish")
     id("signing")
+    id("dev.sigstore.sign")
 }
 
 kotlin.jvmToolchain(8)
@@ -20,31 +21,11 @@ publishing {
     publications.register<MavenPublication>("gpr") {
         from(components["java"])
     }
-    publications.withType<MavenPublication>().configureEach {
-        this.pom {
-            // https://github.com/gradle/gradle/issues/28759
-            this.withXml {
-                this.asNode().appendNode("distributionManagement").appendNode("repository").apply {
-                    this.appendNode("id", "github")
-                    this.appendNode("name", "GitHub hfhbd Apache Maven Packages")
-                    this.appendNode("url", "https://github.com/hfhbd/adventOfCode")
-                }
-            }
-        }
-    }
 }
 
 java {
     withJavadocJar()
     withSourcesJar()
-}
-
-signing {
-    val signingKey = providers.gradleProperty("signingKey")
-    if (signingKey.isPresent) {
-        useInMemoryPgpKeys(signingKey.get(), providers.gradleProperty("signingPassword").get())
-        sign(publishing.publications)
-    }
 }
 
 tasks.withType<AbstractArchiveTask>().configureEach {
