65be9a073e1f71b45151327e2186987edd5b65cb
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-10-13 16:45:29 +0200

    Use Kotlin 2.1.0-Beta2 (#85)

--- gradle/build-logic/settings.gradle.kts
+++ gradle/build-logic/settings.gradle.kts
@@ -1,6 +1,7 @@
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         mavenCentral()
     }
     versionCatalogs.register("libs") {
