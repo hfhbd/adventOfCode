05a44c8cf38e5cf8655e89872f5a1769f8d7f213
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-04-10 16:27:21 +0200

    Simplify code

--- day1/src/main/kotlin/getDigits.kt
+++ day1/src/main/kotlin/getDigits.kt
@@ -1,6 +1,6 @@
 fun getDigits(input: String) = input.lineSequence().map {
     val digits = it.mapNotNull {
-        if (it.isDigit()) it.digitToInt() else null
+        it.digitToIntOrNull()
     }
     (digits.first() * 10) + digits.last()
 }
