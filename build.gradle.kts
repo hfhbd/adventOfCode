3e2657f1d7a95f652a79cd80434ec77553fb6978
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-11-14 13:52:25 +0100

    Add detekt merge plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,16 @@
-import dev.detekt.gradle.report.ReportMergeTask
-
 plugins {
     id("io.github.hfhbd.mavencentral.close")
+    id("merge-detekt")
 }
 
 tasks.closeMavenCentral {
     namespace.set("io.github.hfhbd")
 }
 
-interface SarifDependencies : Dependencies {
-    val sarif: DependencyCollector
-}
-
-val sarifDependencies = objects.newInstance<SarifDependencies>()
-sarifDependencies.apply {
-    for (subproject in subprojects) {
-        sarif(dependencyFactory.create(subproject))
+mergeDetekt {
+    dependencies {
+        for (subproject in subprojects) {
+            sarif(project(subproject.path))
+        }
     }
-}
-
-val sarifFiles = configurations.resolvable("sarifFiles") {
-    fromDependencyCollector(sarifDependencies.sarif)
-    attributes {
-        attribute(Usage.USAGE_ATTRIBUTE, objects.named("detekt-sarif"))
-    }
-}
-
-tasks.register("mergeDetektSarif", ReportMergeTask::class) {
-    input.from(sarifFiles)
-    output.set(layout.buildDirectory.file("reports/detekt/detekt.sarif"))
 }
