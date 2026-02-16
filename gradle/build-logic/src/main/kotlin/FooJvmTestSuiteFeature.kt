d7738e7ed3cdff728d38da51d60631c19b014181
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2026-02-16 12:47:25 +0100

    Use new apply action class (#237)

--- gradle/build-logic/src/main/kotlin/FooJvmTestSuiteFeature.kt
+++ gradle/build-logic/src/main/kotlin/FooJvmTestSuiteFeature.kt
@@ -5,12 +5,13 @@ import org.gradle.features.binding.BuildModel
 import org.gradle.features.binding.Definition
 import org.gradle.features.binding.ProjectFeatureBinding
 import org.gradle.features.binding.ProjectFeatureBindingBuilder
+import org.gradle.features.dsl.bindProjectFeature
 
 @BindsProjectFeature(FooJvmTestSuiteFeature::class)
 abstract class FooJvmTestSuiteFeature : Plugin<Project>, ProjectFeatureBinding {
     override fun apply(target: Project) {}
     override fun bind(builder: ProjectFeatureBindingBuilder) {
-        builder.bindProjectFeature("foo") { _: FooDefinition, _: JvmDclTestSuite ->
+        builder.bindProjectFeature("foo") { _: FooDefinition, _, _: JvmDclTestSuite ->
 
         }
     }
--- gradle/build-logic/src/main/kotlin/JPMSFeature.kt
+++ gradle/build-logic/src/main/kotlin/JPMSFeature.kt
@@ -7,7 +7,6 @@ import org.gradle.features.binding.Definition
 import org.gradle.features.binding.ProjectFeatureApplicationContext
 import org.gradle.features.binding.ProjectFeatureBinding
 import org.gradle.features.binding.ProjectFeatureBindingBuilder
-import org.gradle.features.binding.DeclaredProjectFeatureBindingBuilder
 import org.gradle.features.dsl.bindProjectFeature
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.InputFiles
@@ -15,8 +14,8 @@ import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskContainer
 import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.features.binding.ProjectFeatureApplyAction
 import org.gradle.kotlin.dsl.named
-import org.gradle.kotlin.dsl.newInstance
 import org.gradle.process.CommandLineArgumentProvider
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import javax.inject.Inject
@@ -25,32 +24,34 @@ import javax.inject.Inject
 abstract class JPMSFeature : Plugin<Project>, ProjectFeatureBinding {
     override fun apply(target: Project) {}
     override fun bind(builder: ProjectFeatureBindingBuilder) {
-        builder.bindProjectFeature("jpms") { definition: JPMSDefinition, _: AdventOfCodeDefinition ->
-            with(objectFactory.newInstance(JPMSFeatureAction::class)) {
-                apply(definition)
-            }
-        }.withUnsafeApplyAction()
+        builder.bindProjectFeature("jpms", JPMSFeatureAction::class)
+            .withUnsafeApplyAction()
     }
-}
 
-internal interface JPMSFeatureAction {
-    @get:Inject
-    val tasks: TaskContainer
+    internal interface JPMSFeatureAction : ProjectFeatureApplyAction<JPMSDefinition, BuildModel.None, AdventOfCodeDefinition> {
+        @get:Inject
+        val tasks: TaskContainer
 
-    fun ProjectFeatureApplicationContext.apply(definition: JPMSDefinition) {
-        tasks.named("compileJava", JavaCompile::class) {
-            options.javaModuleVersion.set(project.version.toString().takeUnless { it == DEFAULT_VERSION })
-            options.compilerArgumentProviders += object : CommandLineArgumentProvider {
+        override fun apply(
+            context: ProjectFeatureApplicationContext,
+            definition: JPMSDefinition,
+            buildModel: BuildModel.None,
+            parentDefinition: AdventOfCodeDefinition
+        ) {
+            tasks.named("compileJava", JavaCompile::class) {
+                options.javaModuleVersion.set(project.version.toString().takeUnless { it == DEFAULT_VERSION })
+                options.compilerArgumentProviders += object : CommandLineArgumentProvider {
 
-                @InputFiles
-                @PathSensitive(PathSensitivity.RELATIVE)
-                val kotlinClasses = project.tasks.named("compileKotlin", KotlinCompile::class)
-                    .flatMap { it.destinationDirectory }
+                    @InputFiles
+                    @PathSensitive(PathSensitivity.RELATIVE)
+                    val kotlinClasses = project.tasks.named("compileKotlin", KotlinCompile::class)
+                        .flatMap { it.destinationDirectory }
 
-                override fun asArguments(): List<String> = listOf(
-                    "--patch-module",
-                    "${definition.moduleName.get()}=${kotlinClasses.get().asFile.absolutePath}"
-                )
+                    override fun asArguments(): List<String> = listOf(
+                        "--patch-module",
+                        "${definition.moduleName.get()}=${kotlinClasses.get().asFile.absolutePath}"
+                    )
+                }
             }
         }
     }
@@ -59,18 +60,3 @@ internal interface JPMSFeatureAction {
 interface JPMSDefinition : Definition<BuildModel.None> {
     val moduleName: Property<String>
 }
-
-// https://github.com/gradle/gradle/issues/35870
-public inline fun <
-        reified OwnDefinition : Definition<BuildModel.None>,
-        reified TargetDefinition : Definition<*>,
-        > ProjectFeatureBindingBuilder.bindProjectFeature(
-    name: String,
-    noinline block: ProjectFeatureApplicationContext.(OwnDefinition, TargetDefinition) -> Unit,
-): DeclaredProjectFeatureBindingBuilder<OwnDefinition, BuildModel.None> =
-    bindProjectFeature(name) { definition: OwnDefinition, _: BuildModel.None, target: TargetDefinition ->
-        block(
-            definition,
-            target,
-        )
-    }
